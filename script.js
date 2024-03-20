d3b145de9a138a7a3a5c94d6b6974a6dc75f881b
rafiazharr17 <rafipilo17@gmail.com>
2024-03-20 11:36:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function show() {
+  // Mendapatkan nilai dari input nama (name) dan input asal (asal)
+  var name = document.getElementById("name").value;
+  var asal = document.getElementById("asal").value;
+
+  // Menampilkan pesan popup dengan informasi yang diinginkan
+  alert("Hai " + name + " asal " + asal + ", Salam kenal abanggkuhh🔥");
+}
